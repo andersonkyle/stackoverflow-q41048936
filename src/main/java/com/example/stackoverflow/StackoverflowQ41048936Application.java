98d4b525887977b6978d880edc838f491af9039b
Kyle Anderson <kanderson450@users.noreply.github.com>
2016-12-12 07:23:54 -0800

    Initial Commit

---
+++ src/main/java/com/example/stackoverflow/StackoverflowQ41048936Application.java
@@ -0,0 +1,12 @@
+package com.example.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowQ41048936Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowQ41048936Application.class, args);
+	}
+}
